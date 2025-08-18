0319a1d33b3b594d8c767ea30fb4a73f8e4591e5
gv-1280 <gvc7018@gmail.com>
2025-08-18 17:05:29 +0530

    chunking

--- embeddings.py
+++ embeddings.py
@@ -1,6 +1,25 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
+import os
+import faiss
+import pickle
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+texts = []
+for fname in os.listdir("docs"):
+        with open(os.path.join("docs", fname), "r",encoding="utf8") as f:
+            texts.append(f.read().strip())
+
+#simple chuking
+chunks = []
+for text in texts:
+    for i in range(0, len(text), 500):
+         chunks.append(text[i:i+500])
+         
+#encode chunks
+emb = model.encode(chunks,normalize_embeddings=True)
+emb = np.array(emb, dtype=np.float32)  
 
 sentences = [
     "That is a happy person",
